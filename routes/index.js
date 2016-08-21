49e08eef97ca554f09ddae53831be215a7e86715
baleian <baleian90@gmail.com>
2016-08-21 10:47:59 +0900

    example data file 입출력으로 변경

--- routes/index.js
+++ routes/index.js
@@ -6,41 +6,34 @@ router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-var examples = [{
-  id: 1,
-  title: "Bubble Sort",
-  code: "#include <stdio.h>\n\nint main() {\n\tprintf(\"Bubble Sort\");\n\treturn 0;\n}",
-  input: "",
-  designer: {}
-},
-{
-  id: 2,
-  title: "DFS Searching",
-  code: "#include <stdio.h>\n\nint main() {\n\tprintf(\"DFS Searching\");\n\treturn 0;\n}",
-  input: "",
-  designer: {}
-},
-{
-  id: 3,
-  title: "BFS Searching",
-  code: "#include <stdio.h>\n\nint main() {\n\tprintf(\"BFS Searching\");\n\treturn 0;\n}",
-  input: "",
-  designer: {}
-}];
+
+
+
+var fs = require('fs');
 
 router.get('/api/examples', function (req, res) {
-  res.json(examples.map(function (item) {
-    return {
-      id: item.id,
-      title: item.title
-    };
-  }));
+  fs.readFile('data/examples.json', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    var examples = JSON.parse(data);
+    res.json(examples.map(function (item) {
+      return {
+        id: item.id,
+        title: item.title
+      };
+    }));  
+  });
 });
 
 router.get('/api/examples/:id', function (req, res) {
-  res.json(examples.find(function (item) {
-    return item.id == req.params.id;
-  }));
+  fs.readFile('data/examples.json', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    var examples = JSON.parse(data);
+    res.json(examples.find(function (item) {
+      return item.id == req.params.id;
+    }));
+  });
 });
 
 module.exports = router;
